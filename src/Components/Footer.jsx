fa6c691e7b740178e30338cf38e44ca09d5e53ef
Sayyad Azhar <80194496+Azhar221@users.noreply.github.com>
2021-12-25 23:16:52 +0530

    Footer Component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <>
+            <div id="footer">
+                <footer>
+                    <p>Copyright 2022. All Rights Reserved by Azhar</p>
+                </footer>
+            </div>
+        </>
+    )
+}
