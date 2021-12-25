02a46cce5676e7537d76b54cc9ca56890a50f0df
Sayyad Azhar <80194496+Azhar221@users.noreply.github.com>
2021-12-25 23:16:07 +0530

    About and Contact Component

---
+++ src/Components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <>
+            <div className="about">
+                <h1>About us</h1>
+                <div className="container">
+                    <h1>Azhar Sabir</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, qui magnam? Lorem ipsum, dolor sit
+                        amet
+                        consectetur adipisicing elit. Sapiente vero ex repellendus excepturi quisquam deleniti esse
+                        deserunt, ipsa at eligendi, accusantium in reiciendis eveniet ullam officia non! Necessitatibus,
+                        dignissimos iure?
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
